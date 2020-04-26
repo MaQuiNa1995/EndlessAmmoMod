f9721a3d149dde22c63954fdb9e9ee1c03fefb42
MaQuiNa1995 <maquina1995@gmail.com>
2020-04-26 21:49:02 +0200

    Añadido un arma mágica que lanza planetas con animación de 4 frames

---
+++ Items/Weapons/LanzaPlanetas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FirstMod.Items.Weapons
+{
+    public class LanzaPlanetas : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lanzador De Planetas");
+            Tooltip.SetDefault("Lanza planetas a tus enemigos !!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 80;
+            item.magic = true;                     //this make the item do magic damage
+            item.width = 24;
+            item.height = 28;
+            item.useTime = 30;
+            item.useAnimation = 30;
+            item.useStyle = 5;        //this is how the item is holded
+            item.noMelee = true;
+            item.knockBack = 2;
+            item.value = 1000;
+            item.rare = 3;
+            item.mana = 8;             //mana use
+            item.UseSound = SoundID.Item21;            //this is the sound when you use the item
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("Planeta");  //this make the item shoot your projectile
+            item.shootSpeed = 9f;    //projectile speed when shoot
+        }
+    }
+}
