1f1ad5e3cbe270baed55ead333128d9fbc71ecb3
MaQuiNa1995 <maquina1995@gmail.com>
2021-06-27 01:11:01 +0200

    Creación de la vía de vagoneta infinita y antorchas infinitas

---
+++ Items/Blocks/Creator/UnlimitedIMinecartTrackCreator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MaquiMod.Items.Blocks.Creator
+{
+    public class UnlimitedIMinecartTrackCreator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Rail Infinito");
+            Tooltip.SetDefault("Este rail de vagoneta nunca se acaba !!!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 1;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 4;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = false;
+            item.createTile = TileID.MinecartTrack;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.MinecartTrack,500);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
---
+++ Items/Blocks/Creator/UnlimitedTorchCreator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MaquiMod.Items.Blocks.Creator
+{
+    public class UnlimitedTorchCreator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Antorcha Infinita");
+            Tooltip.SetDefault("Esta madera nunca se acaba !!!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 1;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = false;
+            item.createTile = TileID.Torches;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Torch,500);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
